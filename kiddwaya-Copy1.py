2c28c294aced77aa6cbce2b1c49edd9ab294bb64
Francis Adedayo Atoyebi <40906388+francisatoyebi@users.noreply.github.com>
2020-09-13 17:42:42 +0100

    update

---
+++ kiddwaya-Copy1.py
@@ -0,0 +1,15 @@
+import twint
+import nest_asyncio
+
+
+nest_asyncio.apply()
+
+c = twint.Config()
+
+c.Search = 'kiddwaya'
+c.Since = '2020-09-10 00:00:00'
+c.Until = '2020-09-12 23:59:59'
+c.Store_csv = True
+c.Output = 'Scraped_tweets/kiddwaya.csv'
+
+twint.run.Search(c)
